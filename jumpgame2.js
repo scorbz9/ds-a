932d6501d8665b5d5e2030e1b23e47e45c01515f
Seth Corbett <scorbz9@gmail.com>
2022-07-08 20:41:47 -0700

    jumpgame2 solution

--- jumpgame2.js
+++ jumpgame2.js
@@ -26,4 +26,40 @@
 
 var jump = function(nums) {
 
+    if (nums.length <= 1) return 0;
+
+    // ladder is the farthest right index we can reach from a given index
+    let ladder = nums[0];
+    // steps keep track of when we reach the end of the current ladder
+    let stairs = nums[0];
+
+    let jumpCount = 1;
+
+    for (let i = 1; i < nums.length; i += 1) {
+        console.log('index =', i, 'ladder =', ladder, 'stairs =', stairs)
+        // case for when we perfectly have enough steps to reach end of array
+        if (i === nums.length - 1) {
+            console.log('end of array! result =', jumpCount)
+            return jumpCount;
+        }
+
+        // keeps track of farthest right index possible that we've encountered
+        if (nums[i] + i > ladder) {
+            ladder = nums[i] + i;
+        }
+
+        // keeps track of progress through current ladder;
+        stairs--;
+
+        // when we reach the end of current ladder, we jump to the longest one we've found
+        if (stairs === 0) {
+            jumpCount += 1;
+            console.log('new jump! total jumps =', jumpCount)
+            stairs = ladder - i;
+            console.log('new stairs count =', stairs)
+        }
+    }
+
+    return jumpCount;
+
 };
